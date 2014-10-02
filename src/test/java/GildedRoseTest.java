120f307ec23331ca3a47a643511ea3e744c2b856
Paul Manning <psmanningjr@gmail.com>
2014-10-02 16:38:32 -0400

    quality value decreased by 2 after sell in date passes quality value does not go negative

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -7,20 +7,49 @@ import org.junit.Test;
 
 
 public class GildedRoseTest {
+	
+	List<Item> goods;
+	
+	private void createOneItemList(Item item) {
+		goods = new ArrayList<Item>();
+		goods.add(item);
+	}
 
 	@Test
-	public void testSellInValueDecreaseEachDayForNormalItem() {
-		List<Item> goods = new ArrayList<Item>();
-		goods.add(new Item("Conjured Mana Cake", 3, 6));
+	public void WithNormalItemSellInValueDecreaseByOneEachDay() {
+		createOneItemList(new Item("Conjured Mana Cake", 3, 6));
+
 		GildedRose.updateQuality(goods);
+		
 		assertEquals(2,goods.get(0).getSellIn());
 	}
 
 	@Test
-	public void testQualityValueDecreaseEachDayForNormalItem() {
-		List<Item> goods = new ArrayList<Item>();
-		goods.add(new Item("Conjured Mana Cake", 3, 6));
+	public void WithNormalItemQualityValueDecreaseByOneEachDay() {
+		createOneItemList(new Item("Conjured Mana Cake", 3, 6));
+		
 		GildedRose.updateQuality(goods);
+		
 		assertEquals(5,goods.get(0).getQuality());
 	}
+	
+	@Test
+	public void WithNormalItemQualityValueDecreaseByTwoAfterSellInPasses() {
+		createOneItemList(new Item("Conjured Mana Cake", 1, 6));
+
+		GildedRose.updateQuality(goods);
+		GildedRose.updateQuality(goods);
+		assertEquals(3,goods.get(0).getQuality());	
+	}
+	
+	@Test
+	public void WithNormalItemQualityValueDoesNotGoNegative() {
+		createOneItemList(new Item("Conjured Mana Cake", 1, 1));
+
+		GildedRose.updateQuality(goods);
+		assertEquals(0,goods.get(0).getQuality());	
+		GildedRose.updateQuality(goods);
+		assertEquals(0,goods.get(0).getQuality());	
+	}
+	
 }
