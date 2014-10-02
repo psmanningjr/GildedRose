85b4edfd59c051fbd27bacf93c7e490e1c0ed8a5
Paul Manning <psmanningjr@gmail.com>
2014-10-02 16:25:16 -0400

    test that normal items decrease in sellIn and quality after a day

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,26 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 
 public class GildedRoseTest {
 
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void testSellInValueDecreaseEachDayForNormalItem() {
+		List<Item> goods = new ArrayList<Item>();
+		goods.add(new Item("Conjured Mana Cake", 3, 6));
+		GildedRose.updateQuality(goods);
+		assertEquals(2,goods.get(0).getSellIn());
+	}
+
+	@Test
+	public void testQualityValueDecreaseEachDayForNormalItem() {
+		List<Item> goods = new ArrayList<Item>();
+		goods.add(new Item("Conjured Mana Cake", 3, 6));
+		GildedRose.updateQuality(goods);
+		assertEquals(5,goods.get(0).getQuality());
 	}
 }
